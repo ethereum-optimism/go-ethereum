426561e90462fe84617b51edfc4a29675330cedf
Karl Floersch <karl@karlfloersch.com>
2020-04-20 00:21:40 -0400

    Update genesis.go

--- core/genesis.go
+++ core/genesis.go
@@ -393,8 +393,10 @@ func DeveloperGenesisBlock(period uint64, faucet common.Address) *Genesis {
 	return &Genesis{
 		Config:     &config,
 		ExtraData:  append(append(make([]byte, 32), faucet[:]...), make([]byte, crypto.SignatureLength)...),
-		// GasLimit:   6283185,
-		GasLimit:   471238800,
+		// Changed GasLimit & added ChainID
+		// GasLimit:   6283185, // Old value
+		GasLimit:   471238800, // New value
+		ChainID: big.NewInt(108), // New value
 		Difficulty: big.NewInt(1),
 		Alloc: map[common.Address]GenesisAccount{
 			common.BytesToAddress([]byte{1}): {Balance: big.NewInt(1)}, // ECRecover
