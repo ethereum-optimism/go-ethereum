6f17ce5f0ca09a2688662b8a7debc3e3b5fa6cde
Will Meister <william.k.meister@gmail.com>
2020-05-20 12:53:22 -0500

    fixing linting

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -273,13 +273,14 @@ func (ethash *Ethash) verifyHeader(chain consensus.ChainReader, header, parent *
 		return fmt.Errorf("invalid gasUsed: have %d, gasLimit %d", header.GasUsed, header.GasLimit)
 	}
 
-	// Verify that the gas limit remains within allowed bounds
-	diff := int64(parent.GasLimit) - int64(header.GasLimit)
-	if diff < 0 {
-		diff *= -1
-	}
-
 	// TODO: UNCOMMENT THIS CHECK WHEN WE UNDERSTAND OUR GAS LIMIT REQUIREMENTS
+
+	// Verify that the gas limit remains within allowed bounds
+	//diff := int64(parent.GasLimit) - int64(header.GasLimit)
+	//if diff < 0 {
+	//	diff *= -1
+	//}
+
 	//limit := parent.GasLimit / params.GasLimitBoundDivisor
 	//if uint64(diff) >= limit || header.GasLimit < params.MinGasLimit {
 	//	return fmt.Errorf("invalid gas limit: have %d, want %d += %d", header.GasLimit, parent.GasLimit, limit)
