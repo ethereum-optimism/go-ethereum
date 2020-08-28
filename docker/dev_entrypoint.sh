075675412df030ab064674ebea236a0053ccd00e
Will Meister <william.k.meister@gmail.com>
2020-08-28 08:41:44 -0500

    making geth command more readable

--- docker/dev_entrypoint.sh
+++ docker/dev_entrypoint.sh
@@ -15,4 +15,16 @@ fi
 echo "Starting Geth..."
 ## Command to kick off geth
 TARGET_GAS_LIMIT=${TARGET_GAS_LIMIT:-4294967295}
-./build/bin/geth --dev --datadir $VOLUME_PATH --rpc --rpcaddr $HOSTNAME --rpcvhosts='*' --rpcport $PORT --networkid $NETWORK_ID --rpcapi 'eth,net' --gasprice '0' --targetgaslimit $TARGET_GAS_LIMIT --nousb --gcmode=archive --verbosity "6"
+./build/bin/geth --dev \
+  --datadir $VOLUME_PATH \
+  --rpc \
+  --rpcaddr $HOSTNAME \
+  --rpcvhosts='*' \
+  --rpcport $PORT \
+  --networkid $NETWORK_ID \
+  --rpcapi 'eth,net' \
+  --gasprice '0' \
+  --targetgaslimit $TARGET_GAS_LIMIT \
+  --nousb \
+  --gcmode=archive \
+  --verbosity "6"
