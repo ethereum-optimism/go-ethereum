c58110ad83dff122ddb3079fa2281a7950b668dd
Mark Tyneway <mark.tyneway@gmail.com>
2020-09-22 21:53:24 -0700

    state: update contracts

--- tests/ovm_test.go
+++ tests/ovm_test.go
@@ -50,15 +50,15 @@ func TestInitialState(t *testing.T) {
 
 	codeHashes := map[string]bool{
 		"0xe5ac91913949a832a99293323b31665ca6bd007bca03154d64e1236aeba0b197": false, // l2ToL1MessagePasser
-		"0xcc7bd5598904db5b838a6b00471ba54fb5c2dc426cc89494d3a0e90f4ffeb505": false, // l1MessageSender
+		"0x08c32c18cdaf8b92c32b2c5cd181f7cba6656d404c3892f264decb22fd3c7510": false, // l1MessageSender
 		"0xe8c7ea1431f29500679b1382b4456796fc3bc1b9e28b87db81843ffc313b5c1a": false, // l1ToL2TransactionQueue
 		"0xeb6841864a7bb7884ae85ade69b0bb164a62a46de81749d9b5ef5716a2a8be0c": false, // safetyTransactionQueue
 		"0xd39c5a5b3b7637c20e47ed8afd352b115256d6d7a4f4e2c3b9c31eb8a715dcf9": false, // canonicalTransactionChain
-		"0xe5a029d654de78fc1a0d49be48148fecbcca22fd4071ea6ae5e93d0da0a79c75": false, // stateCommitmentChain
+		"0xad1736290b4f564f37bae2c6083f4673ef6782670faffadc1339ad54311cd280": false, // stateCommitmentChain
 		"0xab0448158015a88b7858056922ac7dc309d6fa1a1fad33cbe2f6bb6183e1a709": false, // stateManager
-		"0x82a23436493f28362ab1d7ceaad22207bb667021ab3c292518456a5a0d8d1730": false, // executionManager
+		"0x41c5aca1c7140c5a7725b92923bb8078b8eb299bb455932d1f009a9db0a20b42": false, // executionManager
 		"0x438eec98a6a47190006c4165134d48232cc4c3d7df5281bb310efe90846e7af2": false, // safetyChecker
-		"0xba2674085018444d1d6d02ff9cf18d360ee8d46d2773e392d312926ff74da41c": false, // fraudVerifier
+		"0x5598c6213e53b6f5cb5411e5caf67633f0e6df42a44a37c10d896b20580e4c31": false, // fraudVerifier
 		"0xc6e120fbc52b6d76231bea4c12088810b3f2f785cffb4d6e51be9441e7958198": false, // rollupMerkleUtils
 		"0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470": false, // deployment EOA
 		"0xe87f70d10b89a185d4d7dd8cbd4d57dc0dab3e098a6bc6189d02e9e5ab6f7ed2": false, // deployerWhitelist
@@ -69,19 +69,19 @@ func TestInitialState(t *testing.T) {
 	addresses := map[string]bool{
 		"0x00000000000000000000000000000000DEAD0002": false, // l1ToL1TransactionQueue
 		"0x4200000000000000000000000000000000000001": false, // l1MessageSender
-		"0x00000000000000000000000000000000DEad0008": false, // deployerWhitelist
 		"0x00000000000000000000000000000000DEad0003": false, // addressResolver
 		"0x00000000000000000000000000000000DEAD0001": false, // stateManager
 		"0x00000000000000000000000000000000DeAd0006": false, // gasConsumer
 		"0x4200000000000000000000000000000000000000": false, // l2ToL1MessagePasser
 		"0x00000000000000000000000000000000DEaD000b": false, // deployment EOA
 		"0x00000000000000000000000000000000DeAd0000": false, // executionManager
-		"0x00000000000000000000000000000000DeAD0004": false, // safetyTransactionQueue
 		"0x00000000000000000000000000000000DEaD000C": false, // fraudVerifier
-		"0x00000000000000000000000000000000dEad0005": false, // canonicalTransactionChain
-		"0x00000000000000000000000000000000deaD000a": false, // stateCommitmentChain
-		"0x00000000000000000000000000000000deaD0007": false, // safetyChecker
+		"0x00000000000000000000000000000000dEad0005": false, // stateCommitmentChain
+		"0x00000000000000000000000000000000deaD000a": false, // safetyChecker
+		"0x00000000000000000000000000000000deaD0007": false, // fraudVerifier
 		"0x00000000000000000000000000000000deAD000E": false, // rollupMerkleUtils
+		"0x00000000000000000000000000000000DEAD0009": false, // deployerWhitelist
+		"0x00000000000000000000000000000000DeAD000D": false, // canonicalTransactionChain
 	}
 
 	for address, account := range dump.Accounts {
