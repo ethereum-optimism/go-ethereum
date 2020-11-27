955b2e07ba96d7b177048ee1fc9a8c5988a64495
Georgios Konstantopoulos <me@gakonst.com>
2020-11-27 11:43:56 +0200

    feat(diffdb): save if a key was mutated or not

--- diffdb/db_test.go
+++ diffdb/db_test.go
@@ -1,7 +1,6 @@
 package diffdb
 
 import (
-	"fmt"
 	"github.com/ethereum/go-ethereum/common"
 	"math/big"
 	"testing"
@@ -19,20 +18,22 @@ func TestInMemoryDb(t *testing.T) {
 		common.Hash{0x0},
 	}
 	addr := common.Address{0x1}
-	db.SetDiffKey(big.NewInt(1), common.Address{0x1, 0x2}, common.Hash{0x12, 0x13})
-	db.SetDiffKey(big.NewInt(1), addr, hashes[0])
-	db.SetDiffKey(big.NewInt(1), addr, hashes[1])
-	db.SetDiffKey(big.NewInt(1), addr, hashes[2])
-	db.SetDiffKey(big.NewInt(1), common.Address{0x2}, common.Hash{0x99})
-	db.SetDiffKey(big.NewInt(2), common.Address{0x2}, common.Hash{0x98})
+	db.SetDiffKey(big.NewInt(1), common.Address{0x1, 0x2}, common.Hash{0x12, 0x13}, false)
+	db.SetDiffKey(big.NewInt(1), addr, hashes[0], false)
+	db.SetDiffKey(big.NewInt(1), addr, hashes[1], false)
+	db.SetDiffKey(big.NewInt(1), addr, hashes[2], false)
+	db.SetDiffKey(big.NewInt(1), common.Address{0x2}, common.Hash{0x99}, false)
+	db.SetDiffKey(big.NewInt(2), common.Address{0x2}, common.Hash{0x98}, true)
 
 	diff, _ := db.GetDiff(big.NewInt(1))
 	for i := range hashes {
-		if hashes[i] != diff[addr][i] {
+		if hashes[i] != diff[addr][i].Key {
 			t.Fatalf("Did not match")
 		}
 	}
 
-	diff, _ = db.GetDiff(big.NewInt(5))
-	fmt.Println(diff)
+	diff, _ = db.GetDiff(big.NewInt(2))
+	if diff[common.Address{0x2}][0].Mutated != true {
+		t.Fatalf("Did not match mutated")
+	}
 }
