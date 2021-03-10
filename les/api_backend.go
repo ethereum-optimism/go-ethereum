44c333a42ddf6cdf7a6f093bf3ab4a3393e51be9
Georgios Konstantopoulos <me@gakonst.com>
2021-03-10 15:27:00 +0200

    feat: allow configuring the L1GasPrice remotely over a new private RPC namespace

--- les/api_backend.go
+++ les/api_backend.go
@@ -283,7 +283,12 @@ func (b *LesApiBackend) SuggestPrice(ctx context.Context) (*big.Int, error) {
 
 // NB: Non sequencer nodes cannot suggest L1 gas prices.
 func (b *LesApiBackend) SuggestDataPrice(ctx context.Context) (*big.Int, error) {
-	panic("not implemented")
+	panic("SuggestDataPrice not implemented")
+}
+
+// NB: Non sequencer nodes cannot set L1 gas prices.
+func (b *LesApiBackend) SetL1GasPrice(ctx context.Context, gasPrice *big.Int) {
+	panic("SetL1GasPrice is not implemented")
 }
 
 func (b *LesApiBackend) ChainDb() ethdb.Database {
