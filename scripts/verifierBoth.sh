b1ffa6a28a1b0b8b7888b80eba95edfdf1d2da58
Kevin Ho <kevinjho1996@gmail.com>
2021-04-01 09:35:24 -0700

    add verifier start scripts

---
+++ scripts/verifierBoth.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./start.sh --rollup.clienthttp http://192.168.1.90:7878 --datadir ./db-verifier3 --rollup.statedumppath https://raw.githubusercontent.com/ethereum-optimism/regenesis/temp/snxPhase1GenesisExperiments/mainnet/v1_bothEM_and_OVM_ETH.json --verifier --rpcport 8222 --eth1.l1crossdomainmessengeraddress 0xfBE93ba0a2Df92A8e8D40cE00acCF9248a6Fc812
